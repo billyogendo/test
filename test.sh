ded3a38c9f05af7be1c6085ad2dce0974e7b936a
billyogendo <39695428+billyogendo@users.noreply.github.com>
2019-01-22 11:36:54 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-export env.some_var = 'AAAA'
-export AWESOME_FILE='the_file'
-export BUILD_NUMBER='BASTION_123'
-export TICKET_NUMBER="1234"
-export BUILD_ID="BASTION_1.2.3."
-export TARGET_HOST="10.0.0.51"
-export BUILD_DISPLAY_NAME="TARGET-HOST,USER"
+
+echo "# Saving some version properties
+BUILD_VERSION=${BuildVersion}
+BUILD_NODE_NAME=${NODE_NAME}
+SOURCE_JOB=${JOB_NAME}
+" > ${WORKSPACE}/BuildVersion.properties
