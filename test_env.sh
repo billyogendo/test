85a1d36a0235f484b8eae2b9e4128105f631c23e
billyogendo <39695428+billyogendo@users.noreply.github.com>
2019-01-22 12:05:25 +0300

    Update test_env.sh

--- test_env.sh
+++ test_env.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-echo 'export VM_NAME="$BUILD_VERSION"' 
-echo "export AOEU=$(echo aoeu)" 
 
+echo 'export VM_NAME="$JOB_NAME"' > ~/load_env.sh
+echo "export AOEU=$(echo aoeu)" >> ~/load_env.sh
+chmod 750 ~/load_env.sh
