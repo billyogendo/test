3de3e6203320cb5ac19253e3f02e97d2849d7ace
billyogendo <39695428+billyogendo@users.noreply.github.com>
2019-01-23 11:36:02 +0300

    Update test_env.sh

--- test_env.sh
+++ test_env.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-env.REMOTE_HOST="'1234' ssh -o StrictHostKeyChecking=no con@52.212.57.196"
-env.DIR="/tmp/targetfolder"
 env.BUILD_NUMBER="BASTION_123"
 env.TICKET_NUMBER="1234"
 env.BUILD_ID="BASTION_1.2.3."
-env.TARGET_HOST="10.0.0.51"
-env.BUILD_DISPLAY_NAME="TARGET-HOST,USER"
+env.TARGET_HOST="'1234' ssh -o StrictHostKeyChecking=no con@52.212.57.196"
+env.BUILD_DISPLAY_NAME="test"
+env.DIR="/tmp/targetfolder"
 env.DB_URL="hello"
 env.DB_URL2="hello2"
