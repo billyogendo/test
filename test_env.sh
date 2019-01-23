4081226040196c81fd79aed6728b21c2e7285ff4
billyogendo <39695428+billyogendo@users.noreply.github.com>
2019-01-23 10:11:05 +0300

    Update test_env.sh

--- test_env.sh
+++ test_env.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-export BUILD_NUMBER=BASTION_123
-export TICKET_NUMBER="1234"
-export BUILD_ID="BASTION_1.2.3."
-export TARGET_HOST="10.0.0.51"
-export BUILD_DISPLAY_NAME="TARGET-HOST,USER"
+env.DB_URL="hello"
+env.DB_URL2="hello2"
